5d2100014aadffa2ad1e33170517890f5aeddba1
vareni4ek <artem20041090@gmail.com>
2023-05-13 23:09:41 +0300

    comm jopas2

--- DailyQuest/src/main/java/com/example/dailyquest/AddTaskController.java
+++ DailyQuest/src/main/java/com/example/dailyquest/AddTaskController.java
@@ -21,7 +21,7 @@ public class AddTaskController {
     @FXML
     private ComboBox<String> difficultyComboBox;
 
-
+//jopas
     private ObservableList<String> difficultyOptions = FXCollections.observableArrayList("Легко", "Нормально", "Складно");
 
     public void initialize() {
